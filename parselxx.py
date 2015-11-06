a67e333c98d64250430467011ea9bc74c6003274
Oren Fromberg <oren.fromberg@apx-labs.com>
2015-11-06 17:03:24 -0500

    writing database

--- parselxx.py
+++ parselxx.py
@@ -1,25 +1,33 @@
 from os import listdir
 from os.path import isfile, join
+import betacode as betacode
+import sqlite3
+
 import re
 
 def convert_to_unicode(string):
 	return "hi"
 
 mypath = "files"
-
+	
 # get array of files
 lxxfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 # get lines in one lxxfile
-#lines = [line.rstrip('\n') for line in open('files/01.Gen.1.mlxx')]
-lines = [line.rstrip('\n') for line in open('files/14.1Kings.mlxx')]
+lines = [line.rstrip('\n') for line in open('files/01.Gen.1.mlxx')]
+#lines = [line.rstrip('\n') for line in open('files/14.1Kings.mlxx')]
 
 book_name = ""
 chapter_nr = ""
 verse_nr = ""
 word_nr = 0
+count = 0
 word = ""
 prefix = ""
+
+outfile = open('outfile.txt', 'wb')
+
+dbconn = sqlite3.connect("database")
 
 for line in lines:
 	if (re.match(".+ .+:.+", line)):
@@ -29,11 +37,18 @@ for line in lines:
 		verse_nr = a[2]
 		prefix = book_name + ", " + chapter_nr + ", " + verse_nr + ", "
 		word_nr = 0
-		#print (book_name + " " + chapter_nr + " " + verse_nr)
 	elif (re.match(".+", line)):
 		word_nr = word_nr + 1
-		#print (str(word_nr) + " = " + line)
+		count = count + 1
 		a = re.split("\s+", line)
 		word = a[0]
-		unicode_word = convert_to_unicode(word)
-		print(prefix + str(word_nr) +": \"" + word + "  \"" + unicode_word + "\"")
+		encoded_word = str.encode(betacode.transliterate(word))
+		unicode_word = encoded_word.decode('utf-8')
+		sqlcode = "INSERT INTO content VALUES (\'" + str(count) + "\', \'" + book_name + "\', \'" + str(chapter_nr) + "\', \'" + str(verse_nr) + "\', \'" +  str(word_nr) + "\', \'" + unicode_word + "\')"
+		#print(prefix + str(word_nr) +": \"" + word + "  \"" + str(unicode_word) + "\"")
+		#outfile.write(unicode_word.encode('utf-8'))
+		dbconn.execute(sqlcode)
+		dbconn.commit()
+
+dbconn.close()
+outfile.close()
